31b931b0953bd4e32e23af5056240ac7068f0255
SlimSim Stationara Jobb <slimsimapps@gmail.com>
2014-12-13 18:57:16 +0100

    Adding start files

---
+++ main.js
@@ -0,0 +1,52 @@
+/*var toastGroupTemplate = document.querySelector('#toastGroup');
+toastGroupTemplate.showToast = function() {
+  document.querySelector('#toast').show();
+}
+*/
+
+window.onload = function(){
+  console.log("\nready\n\n")
+  document.getElementById('mButton').addEventListener('click', function(){
+    console.log("mButton is clicked...")
+
+    var ml = document.getElementById('markerList');
+
+    var mTime = document.querySelector('#mTime').value;
+    var name = document.querySelector('#mName').value;
+
+    var marker = document.createElement('song-marker');
+    marker.name = name;
+    marker.time = mTime;
+
+    console.log(marker)
+
+    ml.appendChild(marker);
+
+  })
+
+
+  setTimeout(function(){
+    var ml = document.getElementById('markerList');
+
+
+    var markerTerty = document.createElement('song-marker');
+    markerTerty.name = "terty";
+    markerTerty.time = 30;
+    markerTerty.textContent = "super duper dunder kul som dansarna ska visa upp";
+    var markerAT = document.createElement('song-marker');
+    markerAT.name = "AT fyra"
+    markerAT.time = 84
+
+
+
+    console.clear();
+
+    ml.appendChild(markerTerty);
+    ml.appendChild(markerAT);
+
+  }, 1000);
+
+
+
+
+}
